b0614595e62c68f7ee5fc59f503a4ae005f74c50
jzhang <zhangchunzhong@gmail.com>
2017-03-26 22:09:53 +0800

    add kube for leaning k8s

---
+++ etcd/main.go
@@ -0,0 +1,24 @@
+import (
+	"log"
+	"time"
+
+	"github.com/coreos/etcd/embed"
+)
+
+func main() {
+	cfg := embed.NewConfig()
+	cfg.Dir = "default.etcd"
+	e, err := embed.StartEtcd(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer e.Close()
+	select {
+	case <-e.Server.ReadyNotify():
+		log.Printf("Server is ready!")
+	case <-time.After(60 * time.Second):
+		e.Server.Stop() // trigger a shutdown
+		log.Printf("Server took too long to start!")
+	}
+	log.Fatal(<-e.Err())
+}
