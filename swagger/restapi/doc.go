968b771e0253d902a5759aaea3e1cc6baacff6bc
zhangcz <jzh@jzh.community.veritas.com>
2017-04-20 15:49:37 +0800

    add go swagger example

---
+++ swagger/restapi/doc.go
@@ -0,0 +1,23 @@
+/*Package restapi A To Do list application
+
+The product of a tutorial on goswagger.io
+
+
+    Schemes:
+      http
+      https
+    Host: localhost
+    BasePath: /
+    Version: 1.0.0
+
+    Consumes:
+    - application/io.goswagger.examples.todo-list.v1+json
+
+
+    Produces:
+    - application/io.goswagger.examples.todo-list.v1+json
+
+
+swagger:meta
+*/
+package restapi
